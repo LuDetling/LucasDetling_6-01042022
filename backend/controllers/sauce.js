b1b1f4e10ed7c6f3bdada052ab11d8ab83c78301
Lucas <lucas.detling@gmail.com>
2022-04-25 17:27:43 +0200

    avancé projet

--- backend/controllers/sauce.js
+++ backend/controllers/sauce.js
@@ -1,7 +1,7 @@
 const Sauce = require('../models/sauce');
+const fs = require('fs');
 
 exports.getSauces = (req, res, next) => {
-  // const saucesObject = JSON.parse(req.body.);
   Sauce.find()
     .then(sauces => res.status(200).json(sauces))
     .catch(error => res.status(400).json({
@@ -20,7 +20,6 @@ exports.getSauce = (req, res, next) => {
 }
 
 exports.createSauce = (req, res, next) => {
-  console.log(req.body);
   const newSauce = JSON.parse(req.body.sauce);
   const sauce = new Sauce({
     ...newSauce,
@@ -35,14 +34,50 @@ exports.createSauce = (req, res, next) => {
     }));
 };
 
+exports.modifySauce = (req, res, next) => {
+  const newSauce = req.file ? {
+    ...JSON.parse(req.body.sauce),
+    imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
+  } : {
+    ...req.body
+  };
+  Sauce.updateOne({
+      _id: req.params.id
+    }, {
+      ...newSauce,
+      _id: req.params.id
+    })
+    .then(() => res.status(200).json({
+      message: 'Objet modifié !'
+    }))
+    .catch(error => res.status(400).json({
+      error
+    }));
+};
 
-// exports.afficher = async (req, res, next) => {
-//   try {
-//     const sauces = res.json();
-//   } catch (err) {
-//     res.status(500).json({
-//       success: false,
-//       err,
-//     });
-//   }
-// }
+exports.deleteSauce = (req, res, next) => {
+  Sauce.findOne({
+      _id: req.params.id
+    })
+    .then(sauce => {
+      const filename = sauce.imageUrl.split('/images/')[1];
+      fs.unlink(`images/${filename}`, () => {
+        Sauce.deleteOne({
+            _id: req.params.id
+          })
+          .then(() => res.status(200).json({
+            message: 'Objet supprimé !'
+          }))
+          .catch(error => res.status(400).json({
+            error
+          }));
+      })
+    })
+    .catch(error => res.status(500).json({
+      error
+    }))
+};
+
+exports.likeSauce = (req, res, next) => {
+
+}
--- backend/routes/sauce.js
+++ backend/routes/sauce.js
@@ -6,8 +6,10 @@ const auth = require('../middleware/auth');
 const multer = require('../middleware/multer-config');
 
 router.get('/', auth, sauceCtrl.getSauces);
-// router.get('/', auth, multer, sauceCtrl.afficher);
 router.get('/:id', auth, sauceCtrl.getSauce);
 router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, multer, sauceCtrl.likeSauce);
 
 module.exports = router;
